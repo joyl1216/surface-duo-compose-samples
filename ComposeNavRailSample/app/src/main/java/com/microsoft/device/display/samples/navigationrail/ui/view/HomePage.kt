0ecad542c7d6460314cb054b44d97a540cc82a54
Joy Liu <joyliu1@microsoft.com>
2022-04-29 10:21:49 -0700

    Completed NavRail sample project

---
+++ ComposeNavRailSample/app/src/main/java/com/microsoft/device/display/samples/navigationrail/ui/view/HomePage.kt
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+package com.microsoft.device.display.samples.navigationrail.ui.view
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpRect
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import com.microsoft.device.display.samples.navigationrail.models.DataProvider
+import com.microsoft.device.display.samples.navigationrail.ui.components.ItemTopBar
+import com.microsoft.device.dualscreen.twopanelayout.TwoPaneLayout
+import com.microsoft.device.dualscreen.twopanelayout.TwoPaneMode
+import com.microsoft.device.dualscreen.twopanelayout.navigateToPane1
+import com.microsoft.device.dualscreen.twopanelayout.navigateToPane2
+import com.microsoft.device.dualscreen.windowstate.WindowState
+
+@ExperimentalAnimationApi
+@ExperimentalUnitApi
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun NavigationRailApp(windowState: WindowState) {
+    // Set up starting route for navigation in pane 1
+    var currentRoute by rememberSaveable { mutableStateOf(navDestinations[0].route) }
+    val updateRoute: (String) -> Unit = { newRoute -> currentRoute = newRoute }
+
+    // Set up variable to store selected image id
+    var imageId: Int? by rememberSaveable { mutableStateOf(null) }
+    val updateImageId: (Int?) -> Unit = { newId -> imageId = newId }
+
+    NavigationRailAppContent(
+        isDualScreen = windowState.isDualScreen(),
+        isDualPortrait = windowState.isDualPortrait(),
+        isDualLandscape = windowState.isDualLandscape(),
+        foldIsOccluding = windowState.foldIsOccluding,
+        foldBoundsDp = windowState.foldBoundsDp,
+        windowHeight = windowState.windowHeightDp,
+        imageId = imageId,
+        updateImageId = updateImageId,
+        currentRoute = currentRoute,
+        updateRoute = updateRoute
+    )
+}
+
+@ExperimentalUnitApi
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun NavigationRailAppContent(
+    isDualScreen: Boolean,
+    isDualPortrait: Boolean,
+    isDualLandscape: Boolean,
+    foldIsOccluding: Boolean,
+    foldBoundsDp: DpRect,
+    windowHeight: Dp,
+    imageId: Int?,
+    updateImageId: (Int?) -> Unit,
+    currentRoute: String,
+    updateRoute: (String) -> Unit
+) {
+    TwoPaneLayout(
+        paneMode = TwoPaneMode.HorizontalSingle,
+        pane1 = {
+            Pane1(isDualScreen, isDualPortrait, imageId, updateImageId, currentRoute, updateRoute)
+        },
+        pane2 = {
+            Pane2(
+                isDualPortrait = isDualPortrait,
+                isDualLandscape = isDualLandscape,
+                foldIsOccluding = foldIsOccluding,
+                foldBoundsDp = foldBoundsDp,
+                windowHeight = windowHeight,
+                imageId = imageId,
+                updateImageId = updateImageId,
+                currentRoute = currentRoute
+            )
+        },
+    )
+
+    // If only one pane is being displayed, make sure the correct pane is displayed
+    if (!isDualPortrait) {
+        if (imageId != null)
+            navigateToPane2()
+        else
+            navigateToPane1()
+    }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun Pane1(
+    isDualScreen: Boolean,
+    isDualPortrait: Boolean,
+    imageId: Int?,
+    updateImageId: (Int?) -> Unit,
+    currentRoute: String,
+    updateRoute: (String) -> Unit
+) {
+    ShowWithNav(isDualScreen, isDualPortrait, imageId, updateImageId, currentRoute, updateRoute)
+}
+
+@ExperimentalUnitApi
+@ExperimentalMaterialApi
+@Composable
+fun Pane2(
+    isDualPortrait: Boolean,
+    isDualLandscape: Boolean,
+    foldIsOccluding: Boolean,
+    foldBoundsDp: DpRect,
+    windowHeight: Dp,
+    imageId: Int?,
+    updateImageId: (Int?) -> Unit,
+    currentRoute: String,
+) {
+    // Retrieve selected image information
+    val selectedImage = imageId?.let { DataProvider.getImage(imageId) }
+
+    // Set up back press action to return to pane 1 and clear image selection
+    val onBackPressed = {
+        navigateToPane1()
+        updateImageId(null)
+    }
+    BackHandler { if (!isDualPortrait) onBackPressed() }
+
+    ItemDetailView(
+        isDualPortrait = isDualPortrait,
+        isDualLandscape = isDualLandscape,
+        foldIsOccluding = foldIsOccluding,
+        foldBoundsDp = foldBoundsDp,
+        windowHeight = windowHeight,
+        selectedImage = selectedImage,
+        currentRoute = currentRoute
+    )
+    // If only one pane is being displayed, show a "back" icon
+    if (!isDualPortrait) {
+        ItemTopBar(
+            onClick = { onBackPressed() }
+        )
+    }
+}
---
+++ ComposeNavRailSample/app/src/main/java/com/microsoft/device/display/samples/navigationrail/ui/view/ItemDetailView.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+package com.microsoft.device.display.samples.navigationrail.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpRect
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import com.microsoft.device.display.samples.navigationrail.R
+import com.microsoft.device.display.samples.navigationrail.models.Image
+import com.microsoft.device.dualscreen.twopanelayout.navigateToPane1
+
+/**
+ * Show the image and details for the selected gallery item. If no item is selected, show
+ * a placeholder view explaining how to open the detail view.
+ *
+ * @param isDualPortrait: true if device is in dual portrait mode
+ * @param isDualLandscape: true if device is in dual landscape mode
+ * @param foldIsOccluding: true if a fold is present and it occludes content, false otherwise
+ * @param foldBoundsDp: the bounds of a fold in the form of an Android Rect
+ * @param windowHeight: full height in dp of the window this view is being shown in
+ * @param selectedImage: currently selected image
+ * @param currentRoute: current route in gallery NavHost
+ */
+@ExperimentalUnitApi
+@ExperimentalMaterialApi
+@Composable
+fun ItemDetailView(
+    isDualPortrait: Boolean,
+    isDualLandscape: Boolean,
+    foldIsOccluding: Boolean,
+    foldBoundsDp: DpRect,
+    windowHeight: Dp,
+    selectedImage: Image? = null,
+    currentRoute: String,
+) {
+    // Find current gallery
+    val gallerySection = navDestinations.find { it.route == currentRoute }
+
+    // If no images are selected, show "select image" message or navigate back to gallery view
+    if (selectedImage == null) {
+        if (isDualPortrait)
+            PlaceholderView(gallerySection)
+        else
+            navigateToPane1()
+        return
+    }
+
+    // Show the image at the top and the details drawer at the bottom
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        ItemImage(Modifier.align(Alignment.TopCenter), selectedImage)
+        ItemDetailsDrawer(
+            image = selectedImage,
+            isDualLandscape = isDualLandscape,
+            isDualPortrait = isDualPortrait,
+            foldIsOccluding = foldIsOccluding,
+            foldBoundsDp = foldBoundsDp,
+            windowHeight = windowHeight,
+            gallerySection = gallerySection,
+        )
+    }
+}
+
+@Composable
+private fun ItemImage(modifier: Modifier, image: Image) {
+    Image(
+        painter = painterResource(id = image.image),
+        contentDescription = stringResource(R.string.image_description, image.name, image.id),
+        modifier = modifier.fillMaxWidth(),
+        contentScale = ContentScale.FillWidth,
+    )
+}
